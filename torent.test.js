ce1dacbb50110192b315ab76ba9a4e8483cbed5c
loopteam0 <loopteam0@gmail.com>
2019-02-03 12:14:21 +0000

    =v0.0.2 pre-release

--- torent.test.js
+++ torent.test.js
@@ -1,5 +1,7 @@
-var tor = require('torrent-search-api');
-tor.enableProvider('1337x');
-// tslint:disable-next-line:no-unused-expression
-var torrents = tor.search('1080', 'Movies', 20).then(function (res) { return console.log(res); });
-console.log(torrents);
+import PirateBay from 'thepiratebay'
+
+PirateBay.search('Game of Thrones', {
+  category: 205
+})
+.then(results => console.log(results))
+.catch(err => console.log(err))
